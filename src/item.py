d20fbeedbb9cbb93171f35bc2d8497839bd38f67
maxlo <maxloginov2001@mail.ru>
2023-03-13 22:15:21 +0300

    commit item.py and test_item.py

--- src/item.py
+++ src/item.py
@@ -13,18 +13,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-    def calculate_total_price(self) -> float:
+        Item.all.append(self)
+
+    def calculate_total_price(self) -> str:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        pass
+        return f"{self.name}: {self.price} RUB\nВ наличии: {self.quantity} шт."
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = float(self.price * self.pay_rate)
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item():
+    assert Item.all == []
+    assert Item.pay_rate == 1.0
+
+
