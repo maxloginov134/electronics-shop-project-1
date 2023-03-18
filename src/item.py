afcc2ab408d4db1846a7f8f6f75a86bf839a6b76
maxlo <maxloginov2001@mail.ru>
2023-03-18 03:50:23 +0300

    commit item.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -25,7 +28,7 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return f"{self.name}: {self.price} RUB\nВ наличии: {self.quantity} шт."
+        return f"{self.__name}: {self.price} RUB\nВ наличии: {self.quantity} шт."
 
     def apply_discount(self) -> float:
         """
@@ -33,3 +36,26 @@ class Item:
         """
         self.price = float(self.price * self.pay_rate)
         return self.price
+
+    @property
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, value):
+        if self.__name > value:
+            return f"Наименование товара должно содержать {value}"
+        return self.__name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r', encoding='windows-1251') as file:
+            data = csv.reader(file)
+            for row in data:
+                cls.all.append(row)
+
+    @staticmethod
+    def string_to_number(any_string: str) -> int:
+        try:
+            return int(any_string)
+        except ValueError:
+            return int(any_string[0: any_string.find('.')])
